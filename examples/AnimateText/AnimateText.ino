57409a9c5b864a33270bc6ea6d397ba7937c1a1d
Daniel Eichhorn <dani.eichhorn@squix.ch>
2015-04-25 22:29:37 +0200

    Adapted example for setNextText

--- examples/AnimateText/AnimateText.ino
+++ examples/AnimateText/AnimateText.ino
@@ -5,11 +5,13 @@
 #define NUMBER_OF_DEVICES 6
 #define CS_PIN 2
 LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CS_PIN);
+int x = 0;
   
 void setup() {
   ledMatrix.init();
 
   ledMatrix.setText("MAX7219 Animation Demo");
+  ledMatrix.setNextText("Second text");
 }
 
 void loop() {
@@ -19,4 +21,8 @@ void loop() {
   ledMatrix.drawText();
   ledMatrix.commit();
   delay(50);
+  x=x+1;
+  if (x == 400) {
+     ledMatrix.setNextText("Third text"); 
+  }
 }
